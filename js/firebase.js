f1381252ae616d6b62c15c5b889a2eeb7d93a409
Francesco Esposito <francesco@MinidiFrancesco.station>
2022-10-19 11:12:54 +0200

    test

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBsjHBJe9nfv8YBMaGbNu0tkmNKcGL-a_U",
-  authDomain: "rememberdiscoparty.firebaseapp.com",
-  projectId: "rememberdiscoparty",
-  storageBucket: "rememberdiscoparty.appspot.com",
-  messagingSenderId: "1031019882194",
-  appId: "1:1031019882194:web:cc2762b673562ac6acaf0b",
-  measurementId: "G-DHLGNSR4T8"
-};
+    apiKey: "AIzaSyB8srW_dVNdUN2q2jSKHFuv11zh2oGjntU",
+    authDomain: "rememberdisco-73206.firebaseapp.com",
+    databaseURL: "https://rememberdisco-73206-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "rememberdisco-73206",
+    storageBucket: "rememberdisco-73206.appspot.com",
+    messagingSenderId: "276922566600",
+    appId: "1:276922566600:web:5961dae5577f4f319ca2fd"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
