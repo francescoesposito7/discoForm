3ff2ebaee463f05bf3f9e57b1a01294dec0b872d
Francesco Esposito <francesco@Mac-mini-di-Francesco.local>
2022-11-02 09:41:01 +0100

    firestore test

--- js/app.js
+++ js/app.js
@@ -7,33 +7,40 @@ const firebaseConfig = {
   messagingSenderId: "232836695865",
   appId: "1:232836695865:web:21f7c8ba162d1b94d84ae8"
 };
-  
+
 firebase.initializeApp(firebaseConfig);
+var firestore = firebase.firestore();
 
-// Firebase Database Reference and the child
-const dbRef = firebase.database().ref();
-const usersRef = dbRef.child('users');
+//Variable to access database collection
+const db = firestore.collection("users");
+
 const addUserBtnUI = document.getElementById("add-user-btn");
-addUserBtnUI.addEventListener("click", addUserBtnClicked)
+addUserBtnUI.addEventListener("click", (e) => {
+  //Prevent Default Form Submission Behavior
+  e.preventDefault();
 
-function addUserBtnClicked() {
+  //Get Form Values
+  let firstName = document.getElementById("name").value;
+  let lastName = document.getElementById("surname").value;
+  let email = document.getElementById("email").value;
+  let date = document.getElementById("date").value;
+  let telephoneNo = document.getElementById("telephoneNo").value;
 
-	const usersRef = dbRef.child('users');
+  db
+  .doc()
+  .set({
+    name: firstName,
+    surnname: lastName,
+    email: email,
+    birthDate: date,
+    telephoneNo: telephoneNo,
+  })
+  .then(() => { })
+  .catch((error) => {
+    console.log(error);
+  });
 
-	const addUserInputsUI = document.getElementsByClassName("user-input");
-
- 	// this object will hold the new user information
-    let newUser = {};
-
-    // loop through View to get the data for the model 
-    for (let i = 0, len = addUserInputsUI.length; i < len; i++) {
-
-        let key = addUserInputsUI[i].getAttribute('data-key');
-        let value = addUserInputsUI[i].value;
-        newUser[key] = value;
-    }
-
-	usersRef.push(newUser)
   document.getElementById("registerForm").reset();
+})
 
-}
+  
