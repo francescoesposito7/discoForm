1f475f87f84c06af26aada20cb25a05144ee0682
Francesco Esposito <francesco@Mac-mini-di-Francesco.local>
2022-11-02 09:25:16 +0100

    firestore test

--- js/appFirestore.js
+++ js/appFirestore.js
@@ -18,20 +18,13 @@ const addUserBtnUI = document.getElementById("add-user-btn");
 addUserBtnUI.addEventListener("click", addUserBtnClicked)
 
 function addUserBtnClicked() {
-    const addUserInputsUI = document.getElementsByClassName("user-input");
-    // this object will hold the new user information
-    let newUser = {};
-
-    // loop through View to get the data for the model 
-    for (let i = 0, len = addUserInputsUI.length; i < len; i++) {
-        let key = addUserInputsUI[i].getAttribute('data-key');
-        let value = addUserInputsUI[i].value;
-        newUser[key] = value;
-    }
-    
     db.doc()
     .set({
-      newUser
+      name: "nome",
+      surnname: "nome",
+      email: "nome",
+      birthDate: "nome",
+      telephoneNo: "nome",
     })
     .then(() => { })
     .catch((error) => {
