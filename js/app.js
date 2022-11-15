6f9041a8dccf3fabe65fe043cdb615e7f3a1a4d8
Francesco Esposito <francesco@Mac-mini-di-Francesco.local>
2022-11-15 10:24:06 +0100

    input tes

--- js/app.js
+++ js/app.js
@@ -82,11 +82,12 @@ addUserBtnUI.addEventListener("click", (e) => {
   var message = `<html>
   <body>
   <h1>REMEMBER PARTY</h1>
-  <p>Verrai inserita/o in una lista nominale all’ingresso del locale.Ti basterà quindi dimostrare la tua identità con un documento in corso di validità e contribuire alla riuscita della serata con una quota di <b>10€</b> da pagare in cassa.</p>
-  <p><b>Orario: 23:30 - 04:30</b><br><b>2011-2022</b></p>
-  <p>Rimani aggiornato, iscriviti al canale Telegram ufficiale: <b>https://t.me/provarem"</b></p>
-  <p>Per qualsiasi richiesta, puoi contattarci: <br></p>
-  <p>WhatsApp: <b>https://wa.me/393486380115</b> <br> Email: <b>remember.party01@gmail.com</b></p>
+  <p><strong>2011-2022</strong></p>
+  <p>Verrai inserita/o in una lista nominale all&rsquo;ingresso del locale.Ti baster&agrave; quindi dimostrare la tua identit&agrave; con un documento in corso di validit&agrave; e contribuire alla riuscita della serata con una quota di <strong>10&euro;</strong> da pagare in cassa.</p>
+  <p><strong>Orario: 23:30 - 04:30</strong></p>
+  <p>Rimani aggiornato, iscriviti al canale Telegram ufficiale: <strong>https://t.me/provarem"</strong></p>
+  <p>Per qualsiasi richiesta, puoi contattarci:</p>
+  <p>WhatsApp: <strong>https://wa.me/393486380115</strong> <br/> Email: <strong>paolochiarella@snackulture.it</strong></p>
   </body>
   </html>`
 
